21ca0e9c1329e285aac51e0476bf8bbb0da7e606
darthjee <darthjee@gmail.com>
2014-02-25 20:30:09 -0300

    Start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotHack::Application.config.secret_key_base = 'dca0a196c3034373234058fc2db7523de428fbc78aca808d63405a887e4c0f8fb9ab9ea301c4cb31ce1d4e4a2c79656f922c7fc9a8ddb56bcb52042f27081aae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DotHack::Application.config.session_store :cookie_store, key: '_dot_hack_session'
