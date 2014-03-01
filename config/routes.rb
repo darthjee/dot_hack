2e80a864542695e935e32340c7d6064327106066
darthjee <darthjee@gmail.com>
2014-03-01 17:35:36 -0300

    Revert "Create User model"
    This reverts commit dfec8dba3aecafcc5f4a66f13624c6a04d646475.
    
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 DotHack::Application.routes.draw do
-  devise_scope :user do
-    get 'login' => "users#login", :as => "login", :defaults=>{:format=>"json"}
-  end
+  get 'login' => "users#login", :as => "login", :defaults=>{:format=>"json"}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
