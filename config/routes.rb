ad1a1b76e0d9bf6f2724c2934cbf1437458b1b69
darthjee <darthjee@gmail.com>
2014-03-01 17:12:44 -0300

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 DotHack::Application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'login' => "users#login", :as => "login", :defaults=>{:format=>"json"}
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
